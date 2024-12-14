a137ea6087e15486f65b48a797dfb50351c4c261
Orino1 <orinomohamed@gmail.com>
2024-12-14 21:53:50 +0100

    feat: using env for sensitive data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 from datetime import timedelta
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 APPEND_SLASH = True
 
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kd=g%jjwmysf=(l-t9lx-yba2()r5st#@pe^ii8xg5-up36#_1"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,11 +88,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "portfolio_django",
-        'USER': 'root',
-        'PASSWORD': 'TestTest123@',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        "NAME": os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -138,6 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
 }
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
