867e2ed37c344b5e9da45f72cc9d83d9a84536a9
Orino1 <orinomohamed@gmail.com>
2024-12-14 16:45:02 +0100

    chore: required flag set to True on all fields

--- backend/admin_auth/serializers.py
+++ backend/admin_auth/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True)
-    password = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True)
+    username = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True, required=True)
+    password = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True, required=True)
 
 
 class PassSerializer(serializers.Serializer):
-    password = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True)
+    password = serializers.CharField(min_length=1, max_length=28, trim_whitespace=True, required=True)
